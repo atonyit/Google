e41bc8c3ccecec080ed959593971da58fc92c57d
Alan <alanitoyah96@gmail.com>
2024-05-12 03:03:16 -0500

    Added functionality for 2 google buttons and changed google to be a gif

---
+++ index.js
@@ -0,0 +1,7 @@
+function linkedIn(){
+    window.open("https://www.linkedin.com/in/alan-tony-itoyah-aa8627262/", "_blank");
+}
+
+function gitHub(){
+    window.open("https://github.com/atonyit/", "_blank");
+}
